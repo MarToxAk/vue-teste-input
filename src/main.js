b592bda10a7076fe63ea9b46ddc482b2173e82df
Junior José <contato@autopyweb.com>
2021-03-10 05:36:30 -0300

    a

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import InputAutoComplete from './InputAutoComplete.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-    render: h => h(InputAutoComplete),
-}).$mount('#app')
