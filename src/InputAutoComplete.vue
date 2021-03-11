bb11c2c53fd32a9ce5f9be649b6c191c084b0d62
Junior José <contato@autopyweb.com>
2021-03-11 02:15:29 -0300

    a

--- src/InputAutoComplete.vue
+++ src/InputAutoComplete.vue
@@ -77,7 +77,12 @@ export default {
                 /*append the DIV element as a child of the autocomplete container:*/
                 this.parentNode.appendChild(a);
                 /*for each item in the array...*/
+                console.log('esse é arr' + arr)
+                console.log('esse é val' + val)
                 for (i = 0; i < arr.length; i++) {
+                    console.log('esse é arr' + arr)
+                    console.log('esse é val' + val)
+                    console.log('esse é i' + i)
                     /*check if the item starts with the same letters as the text field value:*/
                     if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
                         /*create a DIV element for each matching element:*/
